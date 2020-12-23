8e7bedbccc415255c5f82dbe96b8faaf0b726a99
JTTran <tranjt80>
2020-12-23 18:49:44 +0200

    9.20: patientor, step 5

--- patientor/src/components/PatientInfo.tsx
+++ patientor/src/components/PatientInfo.tsx
@@ -23,11 +23,36 @@ const PatientInfo: React.FC<patientProps> = ({ patient }) => {
     return <Icon name="venus" size='large' />;
   };
 
+  const buildDiagnosisList = (diagnosis: string[] | undefined) => {
+    let listItems;
+    if (diagnosis) {
+      listItems = diagnosis.map(dia => {
+        return<li key={dia}>{dia}</li>;
+      });            
+    }    
+    return <ul>{listItems}</ul>;
+  };
+
+  const renderEntries = () => {    
+    let entriesList;
+    if (patient.entries) {
+      entriesList = patient.entries.map(entry => {
+        return <div key={entry.date}>
+          <p >{entry.date} {entry.description}</p>
+          {buildDiagnosisList(entry.diagnosisCodes)}
+        </div>;
+      });
+    }        
+    return entriesList;
+  };
+
   return (
     <div>
       <h1>{patient.name} {getGenderIcon()}</h1>
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+      <h3>entries</h3>
+      {renderEntries()}
     </div>
   );
 };
